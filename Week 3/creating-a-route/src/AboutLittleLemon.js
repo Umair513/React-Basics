51a10663fda09943465ef6d092c54e65a9387531
Umair513 <soomroumair513@gmail.com>
2023-05-05 23:56:24 +0500

    displaying images

---
+++ Week 3/creating-a-route/src/AboutLittleLemon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutLittleLemon() {
+  return (
+    <div>
+      <h1>About AboutLittleLemon</h1>
+    </div>
+  )
+}
+
+export default AboutLittleLemon
---
+++ Week 3/displaying-images/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import logo from "./assets/logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task: Add an image below</h1>
+      <img src={logo} alt="logo"></img>
+    </div>
+  );
+}
+
+export default App;
